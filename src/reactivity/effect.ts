92b6337904ad8965559d2f650d2edea63a7b18a4
yiyang-fairy <1196342044@qq.com>
2023-12-11 20:40:15 +0800

    add

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,72 @@
+import { extend } from "./shared";
+
+class ReactiveEffect {
+  private _fn: any;
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, public scheduler?) {
+    this._fn = fn;
+    this.scheduler = scheduler;
+  }
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+const targetMap = new Map();
+export function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  // key 不能重复, 用set存
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+  if (!activeEffect) return;
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
+}
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
+}
+
+let activeEffect;
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options);
+  _effect.run();
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
+}
