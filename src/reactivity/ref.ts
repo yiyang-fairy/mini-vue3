8b97e8d92d0011c0218828d1594574b68e241741
yiyang-fairy <1196342044@qq.com>
2023-12-15 16:09:00 +0800

    isRef, unRef, proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any;
   private _rawValue: any;
   public dep;
+  public __v_isRef = true;
   constructor(value) {
     // 如果value是对象， 要用reactive包裹
     this._value = convert(value);
@@ -27,6 +28,29 @@ class RefImpl {
 }
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
 }
 
 function trackRefValue(ref) {
