4cce43cd22cbd2115e62fc5ac7c9c45a65ae63e7
yiyang-fairy <1196342044@qq.com>
2023-12-26 21:34:55 +0800

    getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -16,22 +16,28 @@ export function createComponentInstance(vnode: any) {
   component.emit = emit.bind(null, component) as any;
   return component;
 }
+
 export function setupComponent(instance) {
   initProps(instance, instance.vnode.props); // 处理props
   initSlots(instance, instance.vnode.children); // 处理插槽
   setupStatefulComponent(instance);
 }
+
 function setupStatefulComponent(instance) {
   const component = instance.type;
   instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers);
   const { setup } = component;
+
   if (setup) {
+    setCurrentInstance(instance);
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     });
+    setCurrentInstance(null);
     handleSetupResult(instance, setupResult);
   }
 }
+
 function handleSetupResult(instance, setupResult: any) {
   // setupResult 可能时 function 或者 obj
   // 如果是function 就是组件的render函数
@@ -41,7 +47,18 @@ function handleSetupResult(instance, setupResult: any) {
   }
   finishComponentSetup(instance);
 }
+
 function finishComponentSetup(instance: any) {
   const component = instance.type;
   instance.render = component.render;
+}
+
+let currentInstance = null;
+
+export function getCurrentInstance() {
+  return currentInstance;
+}
+
+function setCurrentInstance(instance) {
+  currentInstance = instance;
 }
